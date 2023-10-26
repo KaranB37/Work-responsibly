14777d87f14d909b7ecfbad202b9a24905299e15
KaranB37 <86348214+KaranB37@users.noreply.github.com>
2023-10-27 04:12:31 +0530

    animation

--- src/Section.jsx
+++ src/Section.jsx
@@ -1,70 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+
 import "./Section.css";
 import "./index.css";
 import Social from "./Icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import Typed from "typed.js";
 
 const Section = () => {
-  const [text, setText] = useState("");
+  const typedRef = useRef(null);
   // eslint-disable-next-line
   const [isTyping, setIsTyping] = useState(true);
+  const [showWhiteSheet, setShowWhiteSheet] = useState(true);
 
   useEffect(() => {
-    const textToType = [
-      "A collection of resources for a healthy and balanced approach to work.",
-    ];
-    let currentText = "";
-    let currentIndex = 0;
-    let erase = false;
+    // Hide the white sheet after the animation is done
+    setTimeout(() => {
+      setShowWhiteSheet(false);
+    }, 1500);
 
-    const typeText = () => {
-      if (erase) {
-        currentText = textToType[currentIndex].substring(
-          0,
-          currentText.length - 1
-        );
-      } else {
-        currentText = textToType[currentIndex].substring(
-          0,
-          currentText.length + 1
-        );
-      }
+    // Show the content after the fade-in animation is done
+    setTimeout(() => {
+      setIsTyping(false);
+    }, 3000); // Adjust the timing to match the fade-in animation
 
-      setText(currentText);
-
-      if (!erase && currentText === textToType[currentIndex]) {
-        erase = true;
-        setTimeout(() => {
-          setIsTyping(false);
-          setTimeout(() => {
-            setIsTyping(true);
-            erase = false;
-            currentIndex = (currentIndex + 1) % textToType.length;
-          }, 7000);
-        }, 7000);
-      }
-
-      if (erase && currentText === "") {
-        erase = false;
-        currentIndex = (currentIndex + 1) % textToType.length;
-      }
-
-      const speed = erase ? 40 : 70;
-      setTimeout(typeText, speed);
+    const options = {
+      strings: [
+        "A collection of resources for a healthy and balanced approach to work.",
+      ],
+      typeSpeed: 70, // Typing speed in milliseconds
+      backSpeed: 50, // Erasing speed in milliseconds
+      startDelay: 1000, // Delay before typing starts in milliseconds
+      backDelay: 2000, // Delay before erasing starts in milliseconds
+      loop: true, // Loop the typing animation
+      showCursor: false, // Hide the typing cursor
     };
 
-    typeText();
+    if (typedRef.current) {
+      const typed = new Typed(typedRef.current, options);
 
-    document.addEventListener("DOMContentLoaded", () => {
-      const nav = document.querySelector(".nav");
-
-      nav.classList.add("is-loaded");
-    });
+      return () => {
+        typed.destroy();
+      };
+    }
   }, []);
 
   return (
     <section className="custom-section ">
+      {showWhiteSheet && <div className="white-sheet" />}
       <nav>
         <div className="left">Work Responsibly</div>
         <div className="right">
@@ -75,11 +58,17 @@ const Section = () => {
 
       <div className="content-wrapper">
         <h1>Work Responsibly</h1>
-        <p>— {text}</p>
+        <p>
+          <span className="constant-text">— </span>
+          <span ref={typedRef}></span>
+          <span className="typed-cursor"></span>
+        </p>
       </div>
       <div className="down">
-        <FontAwesomeIcon className="icon" icon={faArrowDown} />
-        <span>Browse Topic and Resources</span>
+        <span>
+          <FontAwesomeIcon className="icon" icon={faArrowDown} /> Browse Topic
+          and Resources
+        </span>
       </div>
       <Social />
     </section>
